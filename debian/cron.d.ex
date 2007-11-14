7705288bc3b6ec38e8524aec70841ebbb9e8c439
herd <herd@4ae5aa78-3208-4169-9a7a-46f8b32eb844>
2007-11-14 21:33:52 +0000

    Debianization, part 1

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the xfce4-squeezebox-plugin package
+#
+0 4	* * *	root	xfce4-squeezebox-plugin_maintenance
---
+++ debian/xfce4-squeezebox-plugin-default.ex
@@ -0,0 +1,10 @@
+# Defaults for xfce4-squeezebox-plugin initscript
+# sourced by /etc/init.d/xfce4-squeezebox-plugin
+# installed at /etc/default/xfce4-squeezebox-plugin by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
