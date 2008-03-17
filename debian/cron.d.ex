b1a558adfd5d7febbab97ddc95697acd43701ba3
herd <herd@4ae5aa78-3208-4169-9a7a-46f8b32eb844>
2008-03-17 19:47:47 +0000

    deleted bogus install stuff (deb)

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the xfce4-squeezebox-plugin package
-#
-0 4	* * *	root	xfce4-squeezebox-plugin_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(xfce4-squeezebox-plugin):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="xfce4-squeezebox-plugin" command="/usr/bin/xfce4-squeezebox-plugin"
